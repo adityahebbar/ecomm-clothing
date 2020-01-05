ff55152b11fe313e6ba40916bea18c3211e32f3c
adityahebbar <aditya_ps@outlook.com>
2020-01-05 11:56:32 +0530

    Moved the Collection preview part from ShopPage to a new component, CollectionOverview.

--- src/pages/shoppage/shoppage.component.js
+++ src/pages/shoppage/shoppage.component.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
     return (
-        <div className="shoppage">
-            {
-                collections.map(collection => <CollectionPreview key={collection.id} {...collection} />)
-            }
-        </div>
+        <CollectionOverview />
     );
 }
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
